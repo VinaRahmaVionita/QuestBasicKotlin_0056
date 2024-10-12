353835e25c7d4430421206600be949c3cf280535
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 15:34:14 +0700

    variable shape dengan mutablelist<string> yang artinya isinya bertipe data string semua menggunakan mutable lis of agar bisa memungkinkan perubahan isi data nya lalu di print line untuk menampilkan hasil nya

--- app/src/main/java/com/example/project1/Collections.kt
+++ app/src/main/java/com/example/project1/Collections.kt
@@ -10,4 +10,10 @@ fun ContohList(){
     //List Read-Only
     val readOnlyAbjad = listOf("A","B","C")
     println(readOnlyAbjad)
+
+    // List Mutable
+    val shape : MutableList<String> =
+        mutableListOf("Circle","Square","Triangle")
+    println(shape)
+
 }
