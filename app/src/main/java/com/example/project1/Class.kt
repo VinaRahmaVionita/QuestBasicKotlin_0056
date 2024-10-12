0b8d9392f51e66016de79cfe1375ea4e9368474a
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 15:20:41 +0700

    fungsi utama untuk menjalankan progam kotlin isi dari kontak yaitu id = 1 dan email = Vina@gmail.com, email bisa diubah sedangkan id tidak bisa, println = print line atau untuk mendisplay output kontak yaitu email

--- app/src/main/java/com/example/project1/Class.kt
+++ app/src/main/java/com/example/project1/Class.kt
@@ -4,6 +4,9 @@ class Motor()
 
 class Contact(val id: Int, var email: String)
 
+fun main() {
+    val contact = Contact(1, "Vina@gmail.com")
+// Prints the value of the property: email println(contact.email)
+    println(contact.email)
 
-
-
+}
