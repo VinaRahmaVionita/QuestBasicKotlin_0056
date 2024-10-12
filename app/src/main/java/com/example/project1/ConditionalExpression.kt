cdff08695b547c761f5d5e30c3e79b744d7f511f
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 16:07:23 +0700

    fungsi conditionalExpression menampilkan === Conditional Expression === val (tidak bisa di ubah) jika number lebih besar dari 0 maka akan menampilkan "Positive Number" jika tidak maka akan menampilkan "Negative Number"

---
+++ app/src/main/java/com/example/project1/ConditionalExpression.kt
@@ -0,0 +1,14 @@
+package com.example.project1
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    // if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+}
