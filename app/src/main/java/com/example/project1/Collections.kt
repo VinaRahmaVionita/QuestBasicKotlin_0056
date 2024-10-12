61bb512c7cd6b8f72974feb64a8b5a5e24f2da11
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 15:50:40 +0700

    MutablesetOf (bisa di ubah) yang isi element nya bertipe data string

--- app/src/main/java/com/example/project1/Collections.kt
+++ app/src/main/java/com/example/project1/Collections.kt
@@ -53,6 +53,11 @@ fun ContohSet() {
     //Set Read-Only
     val readOnlyAbjad = setOf("A", "B", "C")
     println(readOnlyAbjad)
+
+    //Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
 }
 
 
