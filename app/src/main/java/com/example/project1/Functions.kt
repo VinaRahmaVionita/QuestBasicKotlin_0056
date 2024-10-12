71843649866f85f3a125d49dc5a20484edd66eb1
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 16:45:22 +0700

    ini adalah named argument fungsi dengan nama withNamedArgument yang berisi nama bertipe data string dan age bertipe data int printline nya menyertakan parameter nya $name! dan $age!

--- app/src/main/java/com/example/project1/Functions.kt
+++ app/src/main/java/com/example/project1/Functions.kt
@@ -10,3 +10,13 @@ fun withParameter(name: String) {
     println("== withParameter ==")
     println("Hello, $name!")
 }
+
+//named argumen
+//merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age:Int){
+    println()
+    println("== withNamedArgument ==")
+    println("hello, $name! You are $age years old.")
+}
+
+
