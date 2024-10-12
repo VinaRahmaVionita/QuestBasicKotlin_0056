5378c9a0830dc40e92a9e736014bb3af77407914
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 17:14:19 +0700

    mengecek kondisi null jika nevernull sama dengan null maka akan mencetak inferedNonNull id Null jika tidak maka akan mencetak inferredNonNull is not null

--- app/src/main/java/com/example/project1/NullSafety.kt
+++ app/src/main/java/com/example/project1/NullSafety.kt
@@ -12,4 +12,26 @@ fun nullSafety() {
     //this is ok
     nullable = null
 
+    //check for null in conditions
+    if (neverNull == null) {
+        println("inferedNonNull id Null")
+    }
+    else {
+        println("inferredNonNull is not null")
+    }
+
+    //safe call operator
+    println(neverNull.length) //18
+    println(nullable?.length) //null
+
+    //not null dosent accpet null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+}
+
+fun main() {
+    nullSafety()
 }
