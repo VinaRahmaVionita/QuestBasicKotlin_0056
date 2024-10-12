ce4d96a8495bbcc8c4e17d2afe0d3a910a3ba4b5
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-10-12 15:22:37 +0700

    mengupdate isi dari email karena email menggunakan variable var yang berarti bisa di ubah

--- app/src/main/java/com/example/project1/Class.kt
+++ app/src/main/java/com/example/project1/Class.kt
@@ -9,4 +9,8 @@ fun main() {
 // Prints the value of the property: email println(contact.email)
     println(contact.email)
 
+
+// Updates the value of the property: email contact.email = "jane@gmail.com"
+    contact.email = "Rahma@gmail.com"
+
 }
